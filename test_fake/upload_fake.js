8caa39bbe3d36bc179eb0be7d49ee61f6fc81d98
romeucajamba <romeucajamba@gmail.com>
2024-05-23 12:20:03 +0100

    feat:enviado requesições

--- test_fake/upload_fake.js
+++ test_fake/upload_fake.js
@@ -1,34 +1,35 @@
-import { Readable, } from "node:stream";
+import { Readable } from "node:stream";
 
+class ReadableStream extends Readable {
+    // Método obrigatório
+    // Método para retornar os dados dessa Stream
+    index = 1;
 
+    _read() {
+        const number = this.index++;
 
-
-class ReadableStream extends Readable{
-    
-    //Método obrigatório
-    //Método para retornar os dados dessa Stream
-        index = 1
-    _read(){
-        const number = this.index++
-
-       setTimeout(() => {
-            if(number >100)
-                this.push(null)
-            else{
-                 //Convertendo para o formato Buffor
-                 //Buffer só permite String, não number
-                 const buf = Buffer.from(String(number))
-                this.push(buf)
-
-             }
-       }, 1000)
-            
+        setTimeout(() => {
+            if (number > 100) {
+                this.push(null);
+            } else {
+                // Convertendo para o formato Buffer
+                // Buffer só permite String, não number
+                const buf = Buffer.from(String(number));
+                this.push(buf);
+            }
+        }, 1000);
     }
 }
 
-fetch('http://localhost:8000',
-    {
-        method: 'POST',
-        body: new ReadableStream(),
-    }
-)
+fetch('http://localhost:8000', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'text/plain'
+    },
+    body: new ReadableStream,
+    
+}).then(response => {
+    console.log('Response received:', response);
+}).catch(error => {
+    console.error('Error occurred:', error);
+});
