b3f1abe9bf1d70ab0f22cd9331d9b0e3df1b95ad
romeucajamba <romeucajamba@gmail.com>
2024-05-23 23:58:51 +0100

    feat:creating middlewares

---
+++ src/middleware/json.js
@@ -0,0 +1,16 @@
+export function json(request, response){
+
+    const buufer = []
+    
+    for await(const chunk of request){ //Pegando cada pedaço do pacote(Vídeo, audio, arquivo, texto) que vem da minha requisição(Frontend)
+        buufer.push(chunk)//Guardando no meu array
+    }
+
+    try {
+        //Juntando todos os pedaços do pacote enviado e convertendo em string porque o buffer só permite string e convertendo tudo para JSON
+         request.body = JSON.parse(Buffer.concat(buufer).toString())
+    } catch {
+        request.body = null
+    }
+       
+}
--- src/server.js
+++ src/server.js
@@ -3,19 +3,23 @@ import http from "node:http";
 const users = []
 
 
-const server = http.createServer((request, response) => {
+const server = http.createServer(async (request, response) => {
     const { method, url } = request
 
     if(method === "GET" && url === "/users")
         
         return response.setHeader("Content-type", "application/json").end(JSON.stringify(users))
 
-    if(method === "POST" && url === "/users")
+    if(method === "POST" && url === "/users"){
+        const { name, email } = body
+
         users.push({
             id:1,
-            userName: "Romeu Cajamba",
-            email: "romeucajamba@gmail.com"
-    })
+            name,
+            email
+         })
+    }
+       
         return response.writeHead().end("Usuário criado!!")
 })
 
