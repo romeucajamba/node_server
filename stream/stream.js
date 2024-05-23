f08767f935cf035277f7ccd4c1e6c2e702f6cb77
romeucajamba <romeucajamba@gmail.com>
2024-05-23 11:07:27 +0100

    feat:escrevendo dados usando stream do node

--- stream/stream.js
+++ stream/stream.js
@@ -1,3 +1,27 @@
 /// Trabalhando com STREAM
 
-process.stdin.pipe(process.stdout)
+//process.stdin.pipe(process.stdout)
+
+import { Readable } from "node:stream";
+
+class WorkWithStream extends Readable{
+    
+    //Método obrigatório
+    //Método para retornar os dados dessa Stream
+        index = 1
+    _read(){
+        const number = this.index++
+
+        if(number >100)
+            this.push(null)
+        else{
+            //Convertendo para o formato Buffor
+            const buf = Buffer.from(String(number))
+            this.push(buf)
+
+        }
+            
+    }
+}
+
+new WorkWithStream().pipe(process.stdout)
